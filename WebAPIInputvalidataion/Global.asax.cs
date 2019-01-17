d2283e13cd0a62958d3f876c77df41d2e2fb8d25
Øystein Lund <Oystein.Lund@Visma.com>
2019-01-17 15:20:23 +0100

    Cleaning up

--- WebAPIInputvalidataion/Global.asax.cs
+++ WebAPIInputvalidataion/Global.asax.cs
@@ -18,7 +18,7 @@ namespace WebAPIInputvalidataion
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-
+            
         }
     }
 }
